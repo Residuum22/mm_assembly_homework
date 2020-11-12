2c565bacecacde7c5beec00898175ecb50018f8f
Mihalik Márk <mihalik.mark@gmail.com>
2020-11-12 16:26:45 +0100

    Comments are ready
    All the code has commented and small logical changes can appear. It seems to be good.

--- src/main.asm
+++ src/main.asm
@@ -7,6 +7,9 @@
 ;		legkisebb elemének kiválasztása. Minden elem 1 bájtos előjel nélküli szám. 
 ;		Bemenet: tömb kezdőcíme (mutató), elemek száma. Kimenet: az előforduló 
 ;		legkisebb és legnagyobb elem (2 regiszterben).
+; !!!Megjegyzés!!!
+; Elkerülhető lett volna az indirekt címzés használata a programban, de szerettem
+; volna ezt gyakorolni így a kódban lesz indirekt címzés.
 ; -----------------------------------------------------------------------------------
 
 $NOMOD51 ; a sztenderd 8051 regiszter definíciók nem szükségesek
@@ -14,46 +17,44 @@ $NOMOD51 ; a sztenderd 8051 regiszter definíciók nem szükségesek
 
 $INCLUDE (SI_EFM8BB3_Defs.inc) ; regiszter és SFR definiciók
 
-ArrayLength 		EQU R3
 OutputMax_Address 	EQU R0
 OutputMin_Address 	EQU R1
-Temp				EQU R2
+ArrayLength 		EQU R2
+Temp				EQU R3
 ; -----------------------------------------------------------------------------------
 ; Main program előtti inicializáció és keresni kívánt tömb definiálása
 ; -----------------------------------------------------------------------------------
 ; Feladat: Tömb elemeinek definiálása
 ; -----------------------------------------------------------------------------------
 
-; Input tömb regisztrálása a flash memóriában
-InputArray SEGMENT CODE
-RSEG InputArray
-; CSEG InputArray ; Rövidebb megoldás esetleges optimalizálás
+InputArray SEGMENT CODE ; Input tömb regisztrálása valahol flash memóriában
+RSEG InputArray ; és ennek a kiválsztása
 
-Array: DB 215,51,64,74,213,3,11,51 ;Input pálda egy 7 elemű tömb
+Array: DB 215,51,64,74,213,3,11,51 ; Ide kerül a tömb, ne legyen 255 elemnél nagyobb
 ArrayEnd:
 
-DSEG AT 0x20
+DSEG AT 0x30	; 30h felett tárolom a bemenő adatot, így kikerülöm a Bankeket és a Bit Adressable regisztereket. 
 OutputMinRAM: DS 1 ; Data memóriában lefoglalunk 2 regisztert a min és max értékeknek
 OutputMaxRAM: DS 1
 
 ; Ugrótábla létrehozása
-	CSEG AT 0
-	SJMP Main
+CSEG AT 0
+SJMP Main
 
-myprog SEGMENT CODE			;saját kódszegmens létrehozása
-RSEG myprog 				;saját kódszegmens létrehozása
-; ------------------------------------------------------------
+myprog SEGMENT CODE	;saját kódszegmens létrehozása
+RSEG myprog 		;saját kódszegmens létrehozása
+; -----------------------------------------------------------------------------------
 ; Főprogram
-; ------------------------------------------------------------
+; -----------------------------------------------------------------------------------
 ; Feladata: 
-; ------------------------------------------------------------
+; -----------------------------------------------------------------------------------
 Main:
-	CLR IE_EA ; interruptok tilt�sa watchdog tilt�s idej�re
-	MOV WDTCN,#0DEh ; watchdog timer tilt�sa
+	CLR IE_EA ; interruptok tiltása watchdog tiltás idejére
+	MOV WDTCN,#0DEh ; watchdog timer tiltása
 	MOV WDTCN,#0ADh
-	SETB IE_EA ; interruptok enged�lyez�se
+	SETB IE_EA ; interruptok engedélyezése
 
-	CALL SearchMinMax
+	CALL SearchMinMax ; Kereső szubrutin
 	MOV OutputMax_Address, #OutputMaxRAM
 	MOV A, @OutputMax_Address
 	MOV R6, A
@@ -63,48 +64,75 @@ Main:
 EndLoop:
 	JMP $ ; végtelen ciklusban várunk
 
-; -----------------------------------------------------------
-; Sample szubrutin
-; -----------------------------------------------------------
-; Funkció: 		8 bites sz�m maszkol�sa
-; Bementek: 	ArrayLength (R3) - maszkoland� sz�m
-;			 	Temp - maszk
-; Kimenetek:   ArrayLength - maszkolt sz�m
-; Regisztereket m�dos�tja:
-;				A
-; -----------------------------------------------------------
+; -----------------------------------------------------------------------------------
+; SearchMinMax szubrutin
+; -----------------------------------------------------------------------------------
+; Funkció: 		A legkisebb és a legnagyobb elem kikeresése a megadott tömbből
+; Bementek: 	Array kezdőcíme
+; Kimenetek:   	OutputMaxRAM - Legnagyobb elem a DATA memóriában
+;				OutputMinRAM - Legkisebb elem a DATA memóriában
+; Regisztereket módosítja:
+;				A - A regiszter
+;				Temp (R3) - Bank0 R3 regiszter
+;				OutputMax_Address (R0) - Bank0 R0 regiszter
+;				OutputMin_Address (R1) - Bank0 R1 regiszter
+;				ArrayLength (R2) - Bank0 R2 regiszter
+;				DPTR - Data pointer
+;				PSW - Program Status Word
+; -----------------------------------------------------------------------------------
 SearchMinMax:
-	;
 	MOV DPTR, #Array ; Data Pointer-nek átadom a tömb kezdőcímét
-	MOV ArrayLength, #(ArrayEnd-Array)+1 ; A Bank0R3 regiszterben eltárolom a tömb elemszámát
-							  ; Ez az elemszám lesz később a megmaradt elemek száma
-	MOV OutputMax_Address, #OutputMaxRAM ; Az R0-ba eltárolom az OutputMax helyét
-	MOV OutputMin_Address, #OutputMinRAM
-	MOV @OutputMax_Address, #0h
-	MOV @OutputMin_Address, #0xFF
+	MOV ArrayLength, #(ArrayEnd-Array) ; A Bank0 R3 regiszterben eltárolom a tömb elemszámát. Ez az elemszám lesz később a megmaradt elemek száma.
+	MOV OutputMax_Address, #OutputMaxRAM ; Az R0-ban eltárolom az OutputMax helyét
+	MOV OutputMin_Address, #OutputMinRAM ; Az R1-ben eltárolom az OutputMin helyét
+	MOV @OutputMax_Address, #0h ; A legkisebb elem ami lehet 0, szóval betöltöm ezt az értéket, hogy nehogy szemét legyen benne
+	MOV @OutputMin_Address, #0xFF ; Ugyan ez a helyezt, csak a legnagyobb 255
 	TestToAllEllementOfTheArray:
-		DJNZ ArrayLength, MaxTest ; Megvizsgálom az elemszámot, mert ha 0 akkor visszatérhetünk
-		RET
-		MaxTest:
-			MOV A, #0 ; Kinullázom az A regisztert
+		MOV A, ArrayLength ; Vizsgáláshoz betöltöm az A-ba az hosszúságot
+		JNZ TestSubRoutine ; Ha ez nem 0, akkor elkezdem a tesztelést
+		MOV @OutputMax_Address, #0h 
+		MOV @OutputMin_Address, #0h
+		RET ; Ha 0 elemszám visszaugrunk 0 maxmummal és minimummal
+		TestSubRoutine:
+			MOV A, #0 ; Kinullázom az A regisztert, hogy DPTR hozzáadva ne legyen falsch érték
 			MOVC A, @A+DPTR ; Az A regiszterbe belerakom a DPTR helyen lévő kódmemória értéket
-			MOV Temp, A ; Az Temp regisztert egy temp regiszternek használom és belerakom ez az értéket
-			INC DPTR ; Következő elemre lépek a tömben
+			MOV Temp, A ; A Temp(Bank0R3) regiszterbe töltöm a bementi tömbömnek a feldolgozandó elemét
+			INC DPTR ; Következő elemre lépek a tömben, hogy ezt tudjam használni
+			DEC ArrayLength ; Csökkentem az elemszámot, hogy időben végére érjen a program
+			; -------------------------------------------------------------------------
+			; Maximum meghatározó rész
+			; -------------------------------------------------------------------------
+			; Megvalósítás:
+			; Az eddigi legnagyobb értékből kivonom a Temp regiszterben tárolt értéket
+			; és vizsgálom hogy alulcsordult-e az A regiszter, amit a CY flag jelez a 
+			; PSW regiszterben. Ha igen akkor a Tempben tárolt érték nagyobb mint az
+			; eddigi maximum így az új maximum a Temp értéke lesz.
+			; OutputMax-Temp
+			; -------------------------------------------------------------------------
 			CheckMax:
-				MOV A, @OutputMax_Address
+				MOV A, @OutputMax_Address ; Betöltöm a vizsgáláshoz a Max értéket
 				SUBB A, Temp ; Az A-ban lévő számból kivonom az output maxot
 				MOV A, PSW ; A PSW-t előkészítem a vizsgálatra
-				ANL A, #80h ;Összeandelem és ha a az OV bit szerint változtatok ugrést
-				JZ CheckMin ; Ugrok ha az OV 0 mivel 
-				MOV A, Temp ; Temp berakom az a ba
-				MOV @R0, A ; R0 on van az output max
+				ANL A, #80h ;Összeéselem (0b10000000)-val így viszgálom a CY értékét
+				JZ CheckMin ; Ugrok ha a CY 0, vagyis ha nincs alulcsordulás
+				MOV A, Temp ; Ha van alulcsordulás, Temp értékét berakom a Maximum értékének
+				MOV @OutputMax_Address, A
+			; -------------------------------------------------------------------------
+			; Minimum meghatározó rész
+			; -------------------------------------------------------------------------
+			; Megvalósítás:
+			; Hasonlóan működik mint az előző, viszont annyi módosítással, hogy itt 
+			; a Temp értékből vonom ki az előző Minimum értékét és ha ez átcsordul, akkor
+			; a Temp kisebb, mint a Minimum így be kell tölteni a Temp értékét a Minimumba.
+			; Temp-OutputMin
+			; -------------------------------------------------------------------------
 			CheckMin:
 				MOV A, Temp
-				SUBB A, @OutputMin_Address 
-				MOV A, PSW ; A PSW-t előkészítem a vizsgálatra
-				ANL A, #80h ;Összeandelem és ha a az OV bit szerint változtatok ugrést
-				JZ TestToAllEllementOfTheArray ; Ugrok ha az OV 0 mivel
-				MOV A, Temp ; Temp berakom az a ba
-				MOV @OutputMin_Address, A ; R0 on van az output max
+				SUBB A, @OutputMin_Address
+				MOV A, PSW 
+				ANL A, #80h
+				JZ TestToAllEllementOfTheArray
+				MOV A, Temp
+				MOV @OutputMin_Address, A
 				JMP TestToAllEllementOfTheArray
 END
